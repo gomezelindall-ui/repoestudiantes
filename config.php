2b2c9768195cc78410a0555e486deb228aa8484f
gomezelindall-ui <jairovelasquezapi@gmail.com>
2026-09-16 19:39:40 -0500

    cambio2

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configuración de conexión a MySQL (AlwaysData)
-$host = "mysql-elindall.alwaysdata.net";
-$user = "elindall";
-$pass = "jhosep2020";
-$db   = "elindall_repoestudiantes";
+$host = "mysql-jairoapi.alwaysdata.net";
+$user = "jairoapi";
+$pass = "clase1234";
+$db   = "jairoapi_repoestudiantes";
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
